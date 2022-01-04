a2f78756929b2b81b365ee64c1e90ce80f394134
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-01-04 17:10:21 +0100

    move graphql from tfchain repo to this repo

---
+++ generated/modules/enums/enums.ts
@@ -0,0 +1,16 @@
+export enum CertificationType {
+  Diy = 'Diy',
+  Certified = 'Certified',
+}
+export enum ContractState {
+  Created = 'Created',
+  Deleted = 'Deleted',
+  OutOfFunds = 'OutOfFunds',
+}
+export enum DiscountLevel {
+  None = 'None',
+  Default = 'Default',
+  Bronze = 'Bronze',
+  Silver = 'Silver',
+  Gold = 'Gold',
+}
