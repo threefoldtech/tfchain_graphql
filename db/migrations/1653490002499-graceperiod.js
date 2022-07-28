0d875871fb3e5331baf4d043dc03523e1aba82b4
dylanverstraete <dylan_verstraete1@hotmail.com>
2022-07-28 16:40:03 +0200

    try fix

--- db/migrations/1653490002499-graceperiod.js
+++ db/migrations/1653490002499-graceperiod.js
@@ -2,20 +2,20 @@ module.exports = class graceperiod1653490002499 {
   name = 'graceperiod1653490002499'
 
   async up(db) {
-    await db.query(`ALTER TABLE "node_contract" DROP COLUMN "state"`)
+    await db.query(`ALTER TABLE "node_contract" DROP COLUMN "state" CASCADE`)
     await db.query(`ALTER TABLE "node_contract" ADD "state" character varying(11) NOT NULL`)
-    await db.query(`ALTER TABLE "name_contract" DROP COLUMN "state"`)
+    await db.query(`ALTER TABLE "name_contract" DROP COLUMN "state" CASCADE`)
     await db.query(`ALTER TABLE "name_contract" ADD "state" character varying(11) NOT NULL`)
-    await db.query(`ALTER TABLE "rent_contract" DROP COLUMN "state"`)
+    await db.query(`ALTER TABLE "rent_contract" DROP COLUMN "state" CASCADE`)
     await db.query(`ALTER TABLE "rent_contract" ADD "state" character varying(11) NOT NULL`)
   }
 
   async down(db) {
     await db.query(`ALTER TABLE "node_contract" ADD "state" character varying(10) NOT NULL`)
-    await db.query(`ALTER TABLE "node_contract" DROP COLUMN "state"`)
+    await db.query(`ALTER TABLE "node_contract" DROP COLUMN "state" CASCADE`)
     await db.query(`ALTER TABLE "name_contract" ADD "state" character varying(10) NOT NULL`)
-    await db.query(`ALTER TABLE "name_contract" DROP COLUMN "state"`)
+    await db.query(`ALTER TABLE "name_contract" DROP COLUMN "state" CASCADE`)
     await db.query(`ALTER TABLE "rent_contract" ADD "state" character varying(10) NOT NULL`)
-    await db.query(`ALTER TABLE "rent_contract" DROP COLUMN "state"`)
+    await db.query(`ALTER TABLE "rent_contract" DROP COLUMN "state" CASCADE`)
   }
 }
