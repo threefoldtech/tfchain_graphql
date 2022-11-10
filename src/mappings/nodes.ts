1ed0fa49b3ee4f8170cbac7c2afc0dc234cae992
dylanVerstraete <dylan_verstraete1@hotmail.com>
2022-11-10 09:58:07 +0100

    feat: align with v11 types

--- src/mappings/nodes.ts
+++ src/mappings/nodes.ts
@@ -20,6 +20,8 @@ export async function nodeStored(ctx: EventHandlerContext) {
     nodeEvent = node.asV101
   } else if (node.isV105) {
     nodeEvent = node.asV105
+  } else if (node.isV118) {
+    nodeEvent = node.asV118
   }
 
   if (!nodeEvent) {
@@ -36,8 +38,21 @@ export async function nodeStored(ctx: EventHandlerContext) {
   newNode.createdAt = BigInt(ctx.event.blockTimestamp)
   newNode.updatedAt = BigInt(ctx.event.blockTimestamp)
 
-  newNode.country = nodeEvent.country.toString()
-  newNode.city = nodeEvent.city.toString()
+  if (node.isV9) {
+    nodeEvent = node.asV9
+    newNode.country = nodeEvent.country.toString()
+    newNode.city = nodeEvent.city.toString()
+  }
+  if (node.isV28) {
+    nodeEvent = node.asV28
+    newNode.country = nodeEvent.country.toString()
+    newNode.city = nodeEvent.city.toString()
+  }
+  if (node.isV118) {
+    nodeEvent = node.asV118
+    newNode.country = nodeEvent.location.country.toString()
+    newNode.city = nodeEvent.location.city.toString()
+  }
 
   newNode.created = Number(nodeEvent.created)
   newNode.farmingPolicyId = nodeEvent.farmingPolicyId
@@ -62,6 +77,8 @@ export async function nodeStored(ctx: EventHandlerContext) {
 
   if (node.isV43) {
     const nodeAsV43 = node.asV43
+    newNode.country = nodeAsV43.country.toString()
+    newNode.city = nodeAsV43.city.toString()
     newNode.secure = nodeAsV43.secureBoot ? true : false
     newNode.virtualized = nodeAsV43.virtualized ? true : false
     newNode.serialNumber = nodeAsV43.serialNumber.toString()
@@ -78,6 +95,8 @@ export async function nodeStored(ctx: EventHandlerContext) {
     } else {
       return
     }
+    newNode.country = nodeEvent.country.toString()
+    newNode.city = nodeEvent.city.toString()
     newNode.secure = nodeEvent.secureBoot ? true : false
     newNode.virtualized = nodeEvent.virtualized ? true : false
     newNode.serialNumber = nodeEvent.serialNumber.toString()
@@ -129,6 +148,8 @@ export async function nodeUpdated(ctx: EventHandlerContext) {
     nodeEvent = node.asV101
   } else if (node.isV105) {
     nodeEvent = node.asV105
+  } else if (node.isV118) {
+    nodeEvent = node.asV118
   }
 
   if (!nodeEvent) return
@@ -153,8 +174,17 @@ export async function nodeUpdated(ctx: EventHandlerContext) {
     await ctx.store.save<NodeResourcesTotal>(resourcesTotal)
   }
 
-  savedNode.country = nodeEvent.country.toString()
-  savedNode.city = nodeEvent.city.toString()
+  if (node.isV9) {
+    nodeEvent = node.asV9
+    savedNode.country = nodeEvent.country.toString()
+    savedNode.city = nodeEvent.city.toString()
+  }
+
+  if (node.isV118) {
+    nodeEvent = node.asV118
+    savedNode.country = nodeEvent.location.country.toString()
+    savedNode.city = nodeEvent.location.city.toString()
+  }
 
   if (savedNode.location) {
     savedNode.location.latitude = nodeEvent.location.latitude.toString()
@@ -166,6 +196,9 @@ export async function nodeUpdated(ctx: EventHandlerContext) {
 
   if (node.isV28) {
     const nodeAsV28 = node.asV28
+    savedNode.country = nodeAsV28.country.toString()
+    savedNode.city = nodeAsV28.city.toString()
+    
     if (nodeAsV28.certificationType) {
       const certificationTypeAsString = nodeAsV28.certificationType.__kind.toString()
       let certType = NodeCertification.Diy
@@ -185,6 +218,8 @@ export async function nodeUpdated(ctx: EventHandlerContext) {
 
   if (node.isV43) {
     const nodeAsV43 = node.asV43
+    savedNode.country = nodeAsV43.country.toString()
+    savedNode.city = nodeAsV43.city.toString()
     savedNode.secure = nodeAsV43.secureBoot ? true : false
     savedNode.virtualized = nodeAsV43.virtualized ? true : false
     savedNode.serialNumber = nodeAsV43.serialNumber.toString()
@@ -216,6 +251,8 @@ export async function nodeUpdated(ctx: EventHandlerContext) {
     } else {
       return
     }
+    savedNode.country = nodeEvent.country.toString()
+    savedNode.city = nodeEvent.city.toString()
     savedNode.secure = nodeEvent.secureBoot ? true : false
     savedNode.virtualized = nodeEvent.virtualized ? true : false
     savedNode.serialNumber = nodeEvent.serialNumber.toString()
