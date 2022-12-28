3bbbd797173e018cbf36366e589162952f5eb2ea
renauter <renauter@gmail.com>
2022-12-28 09:56:55 -0300

    missing changes from last commit

--- src/mappings/serviceContracts.ts
+++ src/mappings/serviceContracts.ts
@@ -3,7 +3,7 @@ import {
     Store
 } from "@subsquid/substrate-processor";
 import { ServiceContract, ServiceContractState, ServiceContractBill } from "../model";
-import { SmartContractModuleServiceContractCreatedEvent, SmartContractModuleServiceContractApprovedEvent, SmartContractModuleServiceContractCanceledEvent, SmartContractModuleServiceContractBilledEvent } from "../types/events";
+import { SmartContractModuleServiceContractCreatedEvent, SmartContractModuleServiceContractMetadataSetEvent, SmartContractModuleServiceContractFeesSetEvent, SmartContractModuleServiceContractApprovedEvent, SmartContractModuleServiceContractCanceledEvent, SmartContractModuleServiceContractBilledEvent } from "../types/events";
 
 export async function serviceContractCreated(ctx: EventHandlerContext) {
     let serviceContractCreatedEvent = new SmartContractModuleServiceContractCreatedEvent(ctx).asV122
@@ -21,8 +21,50 @@ export async function serviceContractCreated(ctx: EventHandlerContext) {
     serviceContract.acceptedByConsumer = false
     serviceContract.lastBill = BigInt(0)
     serviceContract.state = ServiceContractState.Created
-
     await ctx.store.save<ServiceContract>(serviceContract)
+}
+
+export async function serviceContractMetadataSet(ctx: EventHandlerContext) {
+    let serviceContractMetadataSetEvent = new SmartContractModuleServiceContractMetadataSetEvent(ctx).asV122
+
+    const savedServiceContract = await ctx.store.get(ServiceContract, { where: { serviceContractId: serviceContractMetadataSetEvent.serviceContractId } })
+    if (savedServiceContract) {
+        savedServiceContract.metadata = serviceContractMetadataSetEvent.metadata.toString()
+
+        let state = ServiceContractState.Created
+        switch (serviceContractMetadataSetEvent.state.__kind) {
+            case 'AgreementReady':
+                state = ServiceContractState.AgreementReady
+                break
+            case 'ApprovedByBoth':
+                state = ServiceContractState.ApprovedByBoth
+                break
+        }
+        savedServiceContract.state = state
+        await ctx.store.save<ServiceContract>(savedServiceContract)
+    }
+}
+
+export async function serviceContractFeesSet(ctx: EventHandlerContext) {
+    let serviceContractFeesSetEvent = new SmartContractModuleServiceContractFeesSetEvent(ctx).asV122
+
+    const savedServiceContract = await ctx.store.get(ServiceContract, { where: { serviceContractId: serviceContractFeesSetEvent.serviceContractId } })
+    if (savedServiceContract) {
+        savedServiceContract.baseFee = serviceContractFeesSetEvent.baseFee
+        savedServiceContract.variableFee = serviceContractFeesSetEvent.variableFee
+
+        let state = ServiceContractState.Created
+        switch (serviceContractFeesSetEvent.state.__kind) {
+            case 'AgreementReady':
+                state = ServiceContractState.AgreementReady
+                break
+            case 'ApprovedByBoth':
+                state = ServiceContractState.ApprovedByBoth
+                break
+        }
+        savedServiceContract.state = state
+        await ctx.store.save<ServiceContract>(savedServiceContract)
+    }
 }
 
 export async function serviceContractApproved(ctx: EventHandlerContext) {
@@ -30,10 +72,20 @@ export async function serviceContractApproved(ctx: EventHandlerContext) {
 
     const savedServiceContract = await ctx.store.get(ServiceContract, { where: { serviceContractId: serviceContractApprovedEvent.serviceContractId } })
     if (savedServiceContract) {
-        savedServiceContract.acceptedByService = true
-        savedServiceContract.acceptedByConsumer = true
-        savedServiceContract.state = ServiceContractState.ApprovedByBoth
+        savedServiceContract.acceptedByService = serviceContractApprovedEvent.acceptedByService
+        savedServiceContract.acceptedByConsumer = serviceContractApprovedEvent.acceptedByConsumer
+        savedServiceContract.lastBill = serviceContractApprovedEvent.lastBill
 
+        let state = ServiceContractState.Created
+        switch (serviceContractApprovedEvent.state.__kind) {
+            case 'AgreementReady':
+                state = ServiceContractState.AgreementReady
+                break
+            case 'ApprovedByBoth':
+                state = ServiceContractState.ApprovedByBoth
+                break
+        }
+        savedServiceContract.state = state
         await ctx.store.save<ServiceContract>(savedServiceContract)
     }
 }
@@ -54,10 +106,16 @@ export async function serviceContractBilled(ctx: EventHandlerContext) {
     const serviceContractBill = new ServiceContractBill()
 
     serviceContractBill.id = ctx.event.id
-    serviceContractBill.serviceContractID = serviceContractBilledEvent.serviceContractId
+    serviceContractBill.serviceContractID = serviceContractBilledEvent.serviceContract.serviceContractId
     serviceContractBill.variableAmount = serviceContractBilledEvent.bill.variableAmount
     serviceContractBill.window = serviceContractBilledEvent.bill.window
     serviceContractBill.metadata = serviceContractBilledEvent.bill.metadata.toString()
-
+    serviceContractBill.amount = serviceContractBilledEvent.amount
     await ctx.store.save<ServiceContractBill>(serviceContractBill)
+
+    const savedServiceContract = await ctx.store.get(ServiceContract, { where: { serviceContractId: serviceContractBilledEvent.serviceContract.serviceContractId } })
+    if (savedServiceContract) {
+        savedServiceContract.lastBill = serviceContractBilledEvent.serviceContract.lastBill
+        await ctx.store.save<ServiceContract>(savedServiceContract)
+    }
 }
