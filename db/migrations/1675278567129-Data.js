b7b44664840e9ad95b2b816f2fdf3e37548a3cea
renauter <renauter@gmail.com>
2023-02-02 12:15:08 -0300

    set power target and state default values to Up

--- db/migrations/1675278567129-Data.js
+++ db/migrations/1675278567129-Data.js
@@ -3,6 +3,7 @@ module.exports = class Data1675278567129 {
 
     async up(db) {
         await db.query(`ALTER TABLE "node" ADD "power" jsonb`)
+        await db.query(`ALTER TABLE "node" ALTER COLUMN "power" SET DEFAULT '{"state": "Up", "target": "Up"}'::jsonb`)
     }
 
     async down(db) {
