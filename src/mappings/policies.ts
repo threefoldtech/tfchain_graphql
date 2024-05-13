4b363e1a989d4c8b92308e586e4c182ad75f4e05
Sameh Abouel-saad <samehabouelsaad@gmail.com>
2024-05-13 19:15:11 +0300

    fmt: fix inconsistent use of spaces in indentation

--- src/mappings/policies.ts
+++ src/mappings/policies.ts
@@ -3,187 +3,187 @@ import { Ctx } from "../processor";
 import { EventItem } from '@subsquid/substrate-processor/lib/interfaces/dataSelection'
 
 import {
-  PricingPolicy, FarmingPolicy,
-  Policy, FarmCertification,
-  NodeCertification
+    PricingPolicy, FarmingPolicy,
+    Policy, FarmCertification,
+    NodeCertification
 } from "../model";
 import {
-  TfgridModulePricingPolicyStoredEvent, TfgridModuleFarmingPolicyStoredEvent,
-  TfgridModuleFarmingPolicyUpdatedEvent
+    TfgridModulePricingPolicyStoredEvent, TfgridModuleFarmingPolicyStoredEvent,
+    TfgridModuleFarmingPolicyUpdatedEvent
 } from "../types/events";
 import { validateString } from "./nodes"
 
 export async function pricingPolicyStored(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.PricingPolicyStored', { event: { args: true } }>,
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.PricingPolicyStored', { event: { args: true } }>,
 ) {
-  let pricingPolicyEvent = new TfgridModulePricingPolicyStoredEvent(ctx, item.event)
+    let pricingPolicyEvent = new TfgridModulePricingPolicyStoredEvent(ctx, item.event)
 
-  let pricingPolicyEventParsed
+    let pricingPolicyEventParsed
 
-  if (pricingPolicyEvent.isV49) {
-    pricingPolicyEventParsed = pricingPolicyEvent.asV49
-  } else if (pricingPolicyEvent.isV51) {
-    pricingPolicyEventParsed = pricingPolicyEvent.asV51
-  } else if (pricingPolicyEvent.isV101) {
-    pricingPolicyEventParsed = pricingPolicyEvent.asV101
-  }
+    if (pricingPolicyEvent.isV49) {
+        pricingPolicyEventParsed = pricingPolicyEvent.asV49
+    } else if (pricingPolicyEvent.isV51) {
+        pricingPolicyEventParsed = pricingPolicyEvent.asV51
+    } else if (pricingPolicyEvent.isV101) {
+        pricingPolicyEventParsed = pricingPolicyEvent.asV101
+    }
 
-  if (!pricingPolicyEventParsed) return
+    if (!pricingPolicyEventParsed) return
 
-  let pricingPolicy = new PricingPolicy()
-  pricingPolicy.id = item.event.id
+    let pricingPolicy = new PricingPolicy()
+    pricingPolicy.id = item.event.id
 
-  const savedPolicy = await ctx.store.get(PricingPolicy, { where: { pricingPolicyID: pricingPolicyEventParsed.id } })
-  if (savedPolicy) {
-    pricingPolicy = savedPolicy
-  }
+    const savedPolicy = await ctx.store.get(PricingPolicy, { where: { pricingPolicyID: pricingPolicyEventParsed.id } })
+    if (savedPolicy) {
+        pricingPolicy = savedPolicy
+    }
 
-  pricingPolicy.gridVersion = pricingPolicyEventParsed.version
-  pricingPolicy.pricingPolicyID = pricingPolicyEventParsed.id
-  pricingPolicy.name = validateString(ctx, pricingPolicyEventParsed.name.toString())
-  pricingPolicy.dedicatedNodeDiscount = 0
+    pricingPolicy.gridVersion = pricingPolicyEventParsed.version
+    pricingPolicy.pricingPolicyID = pricingPolicyEventParsed.id
+    pricingPolicy.name = validateString(ctx, pricingPolicyEventParsed.name.toString())
+    pricingPolicy.dedicatedNodeDiscount = 0
 
-  const foundationAccount = ss58.codec("substrate").encode(pricingPolicyEventParsed.foundationAccount);
-  const certifiedSalesAccount = ss58.codec("substrate").encode(pricingPolicyEventParsed.certifiedSalesAccount);
+    const foundationAccount = ss58.codec("substrate").encode(pricingPolicyEventParsed.foundationAccount);
+    const certifiedSalesAccount = ss58.codec("substrate").encode(pricingPolicyEventParsed.certifiedSalesAccount);
 
-  pricingPolicy.foundationAccount = foundationAccount
-  pricingPolicy.certifiedSalesAccount = certifiedSalesAccount
+    pricingPolicy.foundationAccount = foundationAccount
+    pricingPolicy.certifiedSalesAccount = certifiedSalesAccount
 
-  const suPolicy = new Policy()
-  suPolicy.value = pricingPolicyEventParsed.su.value
-  suPolicy.unit = validateString(ctx, pricingPolicyEventParsed.su.unit.toString())
+    const suPolicy = new Policy()
+    suPolicy.value = pricingPolicyEventParsed.su.value
+    suPolicy.unit = validateString(ctx, pricingPolicyEventParsed.su.unit.toString())
 
-  const nuPolicy = new Policy()
-  nuPolicy.value = pricingPolicyEventParsed.nu.value
-  nuPolicy.unit = validateString(ctx, pricingPolicyEventParsed.nu.unit.toString())
+    const nuPolicy = new Policy()
+    nuPolicy.value = pricingPolicyEventParsed.nu.value
+    nuPolicy.unit = validateString(ctx, pricingPolicyEventParsed.nu.unit.toString())
 
-  const cuPolicy = new Policy()
-  cuPolicy.value = pricingPolicyEventParsed.cu.value
-  cuPolicy.unit = validateString(ctx, pricingPolicyEventParsed.cu.unit.toString())
+    const cuPolicy = new Policy()
+    cuPolicy.value = pricingPolicyEventParsed.cu.value
+    cuPolicy.unit = validateString(ctx, pricingPolicyEventParsed.cu.unit.toString())
 
-  const IpuPolicy = new Policy()
-  IpuPolicy.value = pricingPolicyEventParsed.ipu.value
-  IpuPolicy.unit = validateString(ctx, pricingPolicyEventParsed.ipu.unit.toString())
+    const IpuPolicy = new Policy()
+    IpuPolicy.value = pricingPolicyEventParsed.ipu.value
+    IpuPolicy.unit = validateString(ctx, pricingPolicyEventParsed.ipu.unit.toString())
 
-  pricingPolicy.su = suPolicy
-  pricingPolicy.cu = cuPolicy
-  pricingPolicy.nu = nuPolicy
-  pricingPolicy.ipu = IpuPolicy
+    pricingPolicy.su = suPolicy
+    pricingPolicy.cu = cuPolicy
+    pricingPolicy.nu = nuPolicy
+    pricingPolicy.ipu = IpuPolicy
 
-  if (pricingPolicyEvent.isV51) {
-    let policyAsV50 = pricingPolicyEvent.asV51
-    pricingPolicy.dedicatedNodeDiscount = policyAsV50.discountForDedicatedNodes
-  }
+    if (pricingPolicyEvent.isV51) {
+        let policyAsV50 = pricingPolicyEvent.asV51
+        pricingPolicy.dedicatedNodeDiscount = policyAsV50.discountForDedicatedNodes
+    }
 
-  await ctx.store.save<PricingPolicy>(pricingPolicy)
+    await ctx.store.save<PricingPolicy>(pricingPolicy)
 }
 
 export async function farmingPolicyStored(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.FarmingPolicyStored', { event: { args: true } }>,
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.FarmingPolicyStored', { event: { args: true } }>,
 ) {
-  const farmingPolicyEvent = new TfgridModuleFarmingPolicyStoredEvent(ctx, item.event)
+    const farmingPolicyEvent = new TfgridModuleFarmingPolicyStoredEvent(ctx, item.event)
 
-  if (!farmingPolicyEvent.isV63) {
-    return
-  }
+    if (!farmingPolicyEvent.isV63) {
+        return
+    }
 
-  const farmingPolicyStoredEvent = farmingPolicyEvent.asV63
+    const farmingPolicyStoredEvent = farmingPolicyEvent.asV63
 
-  const newFarmingPolicy = new FarmingPolicy()
-  newFarmingPolicy.id = item.event.id
-  newFarmingPolicy.gridVersion = farmingPolicyStoredEvent.version
-  newFarmingPolicy.farmingPolicyID = farmingPolicyStoredEvent.id
-  newFarmingPolicy.name = validateString(ctx, farmingPolicyStoredEvent.name.toString())
+    const newFarmingPolicy = new FarmingPolicy()
+    newFarmingPolicy.id = item.event.id
+    newFarmingPolicy.gridVersion = farmingPolicyStoredEvent.version
+    newFarmingPolicy.farmingPolicyID = farmingPolicyStoredEvent.id
+    newFarmingPolicy.name = validateString(ctx, farmingPolicyStoredEvent.name.toString())
 
-  newFarmingPolicy.cu = farmingPolicyStoredEvent.cu
-  newFarmingPolicy.su = farmingPolicyStoredEvent.su
-  newFarmingPolicy.nu = farmingPolicyStoredEvent.nu
-  newFarmingPolicy.ipv4 = farmingPolicyStoredEvent.ipv4
-  newFarmingPolicy.policyCreated = farmingPolicyStoredEvent.policyCreated
-  newFarmingPolicy.policyEnd = farmingPolicyStoredEvent.policyEnd
-  newFarmingPolicy.immutable = farmingPolicyStoredEvent.immutable
-  newFarmingPolicy.default = farmingPolicyStoredEvent.default
+    newFarmingPolicy.cu = farmingPolicyStoredEvent.cu
+    newFarmingPolicy.su = farmingPolicyStoredEvent.su
+    newFarmingPolicy.nu = farmingPolicyStoredEvent.nu
+    newFarmingPolicy.ipv4 = farmingPolicyStoredEvent.ipv4
+    newFarmingPolicy.policyCreated = farmingPolicyStoredEvent.policyCreated
+    newFarmingPolicy.policyEnd = farmingPolicyStoredEvent.policyEnd
+    newFarmingPolicy.immutable = farmingPolicyStoredEvent.immutable
+    newFarmingPolicy.default = farmingPolicyStoredEvent.default
 
-  const certificationTypeAsString = farmingPolicyStoredEvent.nodeCertification.__kind.toString()
-  let nodeCertType = NodeCertification.Diy
-  switch (certificationTypeAsString) {
-    case 'Diy':
-      nodeCertType = NodeCertification.Diy
-      break
-    case 'Certified':
-      nodeCertType = NodeCertification.Certified
-      break
-  }
-  newFarmingPolicy.nodeCertification = nodeCertType
+    const certificationTypeAsString = farmingPolicyStoredEvent.nodeCertification.__kind.toString()
+    let nodeCertType = NodeCertification.Diy
+    switch (certificationTypeAsString) {
+        case 'Diy':
+            nodeCertType = NodeCertification.Diy
+            break
+        case 'Certified':
+            nodeCertType = NodeCertification.Certified
+            break
+    }
+    newFarmingPolicy.nodeCertification = nodeCertType
 
-  const farmCertificationTypeAsString = farmingPolicyStoredEvent.farmCertification.__kind.toString()
-  let farmCertType = FarmCertification.NotCertified
-  switch (farmCertificationTypeAsString) {
-    case 'NotCertified':
-      farmCertType = FarmCertification.NotCertified
-      break
-    case 'Gold':
-      farmCertType = FarmCertification.Gold
-      break
-  }
-  newFarmingPolicy.farmCertification = farmCertType
+    const farmCertificationTypeAsString = farmingPolicyStoredEvent.farmCertification.__kind.toString()
+    let farmCertType = FarmCertification.NotCertified
+    switch (farmCertificationTypeAsString) {
+        case 'NotCertified':
+            farmCertType = FarmCertification.NotCertified
+            break
+        case 'Gold':
+            farmCertType = FarmCertification.Gold
+            break
+    }
+    newFarmingPolicy.farmCertification = farmCertType
 
-  await ctx.store.save<FarmingPolicy>(newFarmingPolicy)
+    await ctx.store.save<FarmingPolicy>(newFarmingPolicy)
 }
 
 export async function farmingPolicyUpdated(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.FarmingPolicyUpdated', { event: { args: true } }>,
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.FarmingPolicyUpdated', { event: { args: true } }>,
 ) {
-  const farmingPolicyEvent = new TfgridModuleFarmingPolicyUpdatedEvent(ctx, item.event)
+    const farmingPolicyEvent = new TfgridModuleFarmingPolicyUpdatedEvent(ctx, item.event)
 
-  if (!farmingPolicyEvent.isV63) {
-    return
-  }
+    if (!farmingPolicyEvent.isV63) {
+        return
+    }
 
-  const farmingPolicyUpdatedEvent = farmingPolicyEvent.asV63
+    const farmingPolicyUpdatedEvent = farmingPolicyEvent.asV63
 
-  const savedPolicy = await ctx.store.get(FarmingPolicy, { where: { farmingPolicyID: farmingPolicyUpdatedEvent.id } })
-  if (!savedPolicy) return
+    const savedPolicy = await ctx.store.get(FarmingPolicy, { where: { farmingPolicyID: farmingPolicyUpdatedEvent.id } })
+    if (!savedPolicy) return
 
-  savedPolicy.gridVersion = farmingPolicyUpdatedEvent.version
-  savedPolicy.farmingPolicyID = farmingPolicyUpdatedEvent.id
-  savedPolicy.name = validateString(ctx, farmingPolicyUpdatedEvent.name.toString())
+    savedPolicy.gridVersion = farmingPolicyUpdatedEvent.version
+    savedPolicy.farmingPolicyID = farmingPolicyUpdatedEvent.id
+    savedPolicy.name = validateString(ctx, farmingPolicyUpdatedEvent.name.toString())
 
-  savedPolicy.cu = farmingPolicyUpdatedEvent.cu
-  savedPolicy.su = farmingPolicyUpdatedEvent.su
-  savedPolicy.nu = farmingPolicyUpdatedEvent.nu
-  savedPolicy.ipv4 = farmingPolicyUpdatedEvent.ipv4
-  savedPolicy.policyCreated = farmingPolicyUpdatedEvent.policyCreated
-  savedPolicy.policyEnd = farmingPolicyUpdatedEvent.policyEnd
-  savedPolicy.immutable = farmingPolicyUpdatedEvent.immutable
-  savedPolicy.default = farmingPolicyUpdatedEvent.default
+    savedPolicy.cu = farmingPolicyUpdatedEvent.cu
+    savedPolicy.su = farmingPolicyUpdatedEvent.su
+    savedPolicy.nu = farmingPolicyUpdatedEvent.nu
+    savedPolicy.ipv4 = farmingPolicyUpdatedEvent.ipv4
+    savedPolicy.policyCreated = farmingPolicyUpdatedEvent.policyCreated
+    savedPolicy.policyEnd = farmingPolicyUpdatedEvent.policyEnd
+    savedPolicy.immutable = farmingPolicyUpdatedEvent.immutable
+    savedPolicy.default = farmingPolicyUpdatedEvent.default
 
-  const certificationTypeAsString = farmingPolicyUpdatedEvent.nodeCertification.__kind.toString()
-  let nodeCertType = NodeCertification.Diy
-  switch (certificationTypeAsString) {
-    case 'Diy':
-      nodeCertType = NodeCertification.Diy
-      break
-    case 'Certified':
-      nodeCertType = NodeCertification.Certified
-      break
-  }
-  savedPolicy.nodeCertification = nodeCertType
+    const certificationTypeAsString = farmingPolicyUpdatedEvent.nodeCertification.__kind.toString()
+    let nodeCertType = NodeCertification.Diy
+    switch (certificationTypeAsString) {
+        case 'Diy':
+            nodeCertType = NodeCertification.Diy
+            break
+        case 'Certified':
+            nodeCertType = NodeCertification.Certified
+            break
+    }
+    savedPolicy.nodeCertification = nodeCertType
 
-  const farmCertificationTypeAsString = farmingPolicyUpdatedEvent.farmCertification.__kind.toString()
-  let farmCertType = FarmCertification.NotCertified
-  switch (farmCertificationTypeAsString) {
-    case 'NotCertified':
-      farmCertType = FarmCertification.NotCertified
-      break
-    case 'Gold':
-      farmCertType = FarmCertification.Gold
-      break
-  }
-  savedPolicy.farmCertification = farmCertType
+    const farmCertificationTypeAsString = farmingPolicyUpdatedEvent.farmCertification.__kind.toString()
+    let farmCertType = FarmCertification.NotCertified
+    switch (farmCertificationTypeAsString) {
+        case 'NotCertified':
+            farmCertType = FarmCertification.NotCertified
+            break
+        case 'Gold':
+            farmCertType = FarmCertification.Gold
+            break
+    }
+    savedPolicy.farmCertification = farmCertType
 
-  await ctx.store.save<FarmingPolicy>(savedPolicy)
+    await ctx.store.save<FarmingPolicy>(savedPolicy)
 }
--- src/mappings/twins.ts
+++ src/mappings/twins.ts
@@ -8,220 +8,220 @@ import { TfgridModuleTwinStoredEvent, TfgridModuleTwinDeletedEvent, TfgridModule
 import { validateString } from "./nodes";
 
 export async function twinStored(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.TwinStored', { event: { args: true } }>
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.TwinStored', { event: { args: true } }>
 ) {
-  const twin = getTwinCreate(ctx, item)
+    const twin = getTwinCreate(ctx, item)
 
-  const newTwin = new Twin()
+    const newTwin = new Twin()
 
-  newTwin.id = item.event.id
-  newTwin.gridVersion = twin.version
-  newTwin.twinID = twin.twinID
-  newTwin.accountID = twin.accountID
-  newTwin.relay = twin.relay
-  newTwin.publicKey = twin.pk
+    newTwin.id = item.event.id
+    newTwin.gridVersion = twin.version
+    newTwin.twinID = twin.twinID
+    newTwin.accountID = twin.accountID
+    newTwin.relay = twin.relay
+    newTwin.publicKey = twin.pk
 
-  await ctx.store.save<Twin>(newTwin)
+    await ctx.store.save<Twin>(newTwin)
 }
 
 export async function twinUpdated(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.TwinUpdated', { event: { args: true } }>
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.TwinUpdated', { event: { args: true } }>
 ) {
-  const twin = getTwinUpdate(ctx, item)
+    const twin = getTwinUpdate(ctx, item)
 
-  const savedTwin: any = await ctx.store.get(Twin, { where: { twinID: twin.twinID } })
-  if (!savedTwin) return
+    const savedTwin: any = await ctx.store.get(Twin, { where: { twinID: twin.twinID } })
+    if (!savedTwin) return
 
-  savedTwin.relay = twin.relay
-  savedTwin.publicKey = twin.pk
-  savedTwin.gridVersion = twin.version
+    savedTwin.relay = twin.relay
+    savedTwin.publicKey = twin.pk
+    savedTwin.gridVersion = twin.version
 
-  await ctx.store.save<Twin>(savedTwin)
+    await ctx.store.save<Twin>(savedTwin)
 }
 
 export async function twinDeleted(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.TwinDeleted', { event: { args: true } }>
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.TwinDeleted', { event: { args: true } }>
 ) {
-  const twinDeletedEvent = new TfgridModuleTwinDeletedEvent(ctx, item.event).asV49
+    const twinDeletedEvent = new TfgridModuleTwinDeletedEvent(ctx, item.event).asV49
 
-  const savedTwin: any = await ctx.store.get(Twin, { where: { twinID: twinDeletedEvent } })
-  if (!savedTwin) return
+    const savedTwin: any = await ctx.store.get(Twin, { where: { twinID: twinDeletedEvent } })
+    if (!savedTwin) return
 
-  await ctx.store.remove<Twin>(savedTwin)
+    await ctx.store.remove<Twin>(savedTwin)
 }
 
 export async function twinCreateOrUpdateOrDelete(ctx: Ctx): Promise<[Twin[], Twin[], Twin[]]> {
-  let newTwins = []
-  let updatedTwins = []
-  let deletedTwins = []
-  for (let block of ctx.blocks) {
-    for (let item of block.items) {
-      if (item.name === "TfgridModule.TwinStored") {
-        const twin = getTwinCreate(ctx, item)
+    let newTwins = []
+    let updatedTwins = []
+    let deletedTwins = []
+    for (let block of ctx.blocks) {
+        for (let item of block.items) {
+            if (item.name === "TfgridModule.TwinStored") {
+                const twin = getTwinCreate(ctx, item)
 
-        const newTwin = new Twin()
+                const newTwin = new Twin()
 
-        newTwin.id = item.event.id
-        newTwin.gridVersion = twin.version
-        newTwin.twinID = twin.twinID
-        newTwin.accountID = twin.accountID
-        newTwin.relay = twin.relay
-        newTwin.publicKey = twin.pk
+                newTwin.id = item.event.id
+                newTwin.gridVersion = twin.version
+                newTwin.twinID = twin.twinID
+                newTwin.accountID = twin.accountID
+                newTwin.relay = twin.relay
+                newTwin.publicKey = twin.pk
 
-        newTwins.push(newTwin)
-      }
-      if (item.name === "TfgridModule.TwinUpdated") {
-        const twin = getTwinUpdate(ctx, item)
+                newTwins.push(newTwin)
+            }
+            if (item.name === "TfgridModule.TwinUpdated") {
+                const twin = getTwinUpdate(ctx, item)
 
-        const foundInNewListIndex: number = newTwins.findIndex(t => t.twinID == twin.twinID);
-        if (foundInNewListIndex != -1) {
-          const savedTwin: Twin = newTwins[foundInNewListIndex]
-          savedTwin.relay = twin.relay
-          savedTwin.publicKey = twin.pk
-          savedTwin.gridVersion = twin.version
-          newTwins[foundInNewListIndex] = savedTwin
-          continue
+                const foundInNewListIndex: number = newTwins.findIndex(t => t.twinID == twin.twinID);
+                if (foundInNewListIndex != -1) {
+                    const savedTwin: Twin = newTwins[foundInNewListIndex]
+                    savedTwin.relay = twin.relay
+                    savedTwin.publicKey = twin.pk
+                    savedTwin.gridVersion = twin.version
+                    newTwins[foundInNewListIndex] = savedTwin
+                    continue
+                }
+
+                const foundInUpdatedListIndex: number = updatedTwins.findIndex(t => t.twinID == twin.twinID);
+                if (foundInUpdatedListIndex != -1) {
+                    let savedTwin: Twin = updatedTwins[foundInUpdatedListIndex]
+                    savedTwin.relay = twin.relay
+                    savedTwin.publicKey = twin.pk
+                    savedTwin.gridVersion = twin.version
+                    updatedTwins[foundInUpdatedListIndex] = savedTwin
+                    continue
+                }
+
+                const savedTwin: any = await ctx.store.get(Twin, { where: { twinID: twin.twinID } })
+                if (!savedTwin) continue
+                savedTwin.relay = twin.relay
+                savedTwin.publicKey = twin.pk
+                savedTwin.gridVersion = twin.version
+                updatedTwins.push(savedTwin)
+            }
+            if (item.name === "TfgridModule.TwinDeleted") {
+                const twinDeletedEvent = new TfgridModuleTwinDeletedEvent(ctx, item.event).asV49
+
+                const savedTwin = await ctx.store.get(Twin, { where: { twinID: twinDeletedEvent } })
+                if (savedTwin) {
+                    deletedTwins.push(savedTwin)
+                }
+            }
         }
-
-        const foundInUpdatedListIndex: number = updatedTwins.findIndex(t => t.twinID == twin.twinID);
-        if (foundInUpdatedListIndex != -1) {
-          let savedTwin: Twin = updatedTwins[foundInUpdatedListIndex]
-          savedTwin.relay = twin.relay
-          savedTwin.publicKey = twin.pk
-          savedTwin.gridVersion = twin.version
-          updatedTwins[foundInUpdatedListIndex] = savedTwin
-          continue
-        }
-
-        const savedTwin: any = await ctx.store.get(Twin, { where: { twinID: twin.twinID } })
-        if (!savedTwin) continue
-        savedTwin.relay = twin.relay
-        savedTwin.publicKey = twin.pk
-        savedTwin.gridVersion = twin.version
-        updatedTwins.push(savedTwin)
-      }
-      if (item.name === "TfgridModule.TwinDeleted") {
-        const twinDeletedEvent = new TfgridModuleTwinDeletedEvent(ctx, item.event).asV49
-
-        const savedTwin = await ctx.store.get(Twin, { where: { twinID: twinDeletedEvent } })
-        if (savedTwin) {
-          deletedTwins.push(savedTwin)
-        }
-      }
     }
-  }
 
-  return [newTwins, updatedTwins, deletedTwins]
+    return [newTwins, updatedTwins, deletedTwins]
 }
 
 function getTwinCreate(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.TwinStored', { event: { args: true } }>
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.TwinStored', { event: { args: true } }>
 ): TwinEvent {
-  let id = ""
-  let twinID = 0
-  let version = 0
-  let relay = ""
-  let accountID = ""
-  let pk = ""
+    let id = ""
+    let twinID = 0
+    let version = 0
+    let relay = ""
+    let accountID = ""
+    let pk = ""
 
-  const twinEvent = new TfgridModuleTwinStoredEvent(ctx, item.event)
-  let twin
-  if (twinEvent.isV49) {
-    twin = twinEvent.asV49
-    id = item.event.id
-    twinID = twin.id
-    version = twin.version
-    relay = validateString(ctx, twin.ip.toString())
-    accountID = ss58.codec("substrate").encode(twin.accountId)
-  } else if (twinEvent.isV101) {
-    twin = twinEvent.asV101
-    id = item.event.id
-    twinID = twin.id
-    version = twin.version
-    relay = validateString(ctx, twin.ip.toString())
-    accountID = ss58.codec("substrate").encode(twin.accountId)
-  } else if (twinEvent.isV124) {
-    twin = twinEvent.asV124
-    id = item.event.id
-    twinID = twin.id
-    if (twin.relay) {
-      relay = validateString(ctx, twin.relay?.toString())
+    const twinEvent = new TfgridModuleTwinStoredEvent(ctx, item.event)
+    let twin
+    if (twinEvent.isV49) {
+        twin = twinEvent.asV49
+        id = item.event.id
+        twinID = twin.id
+        version = twin.version
+        relay = validateString(ctx, twin.ip.toString())
+        accountID = ss58.codec("substrate").encode(twin.accountId)
+    } else if (twinEvent.isV101) {
+        twin = twinEvent.asV101
+        id = item.event.id
+        twinID = twin.id
+        version = twin.version
+        relay = validateString(ctx, twin.ip.toString())
+        accountID = ss58.codec("substrate").encode(twin.accountId)
+    } else if (twinEvent.isV124) {
+        twin = twinEvent.asV124
+        id = item.event.id
+        twinID = twin.id
+        if (twin.relay) {
+            relay = validateString(ctx, twin.relay?.toString())
+        }
+        accountID = ss58.codec("substrate").encode(twin.accountId)
+        if (twin.pk) {
+            pk = '0x' + Buffer.from(twin.pk).toString('hex');
+        }
     }
-    accountID = ss58.codec("substrate").encode(twin.accountId)
-    if (twin.pk) {
-      pk = '0x' + Buffer.from(twin.pk).toString('hex');
-    }
-  }
 
-  return {
-    id,
-    twinID,
-    version,
-    relay,
-    accountID,
-    pk
-  }
+    return {
+        id,
+        twinID,
+        version,
+        relay,
+        accountID,
+        pk
+    }
 }
 
 function getTwinUpdate(
-  ctx: Ctx,
-  item: EventItem<'TfgridModule.TwinUpdated', { event: { args: true } }>
+    ctx: Ctx,
+    item: EventItem<'TfgridModule.TwinUpdated', { event: { args: true } }>
 ): TwinEvent {
-  let id = ""
-  let twinID = 0
-  let version = 0
-  let relay = ""
-  let accountID = ""
-  let pk = ""
+    let id = ""
+    let twinID = 0
+    let version = 0
+    let relay = ""
+    let accountID = ""
+    let pk = ""
 
-  const twinEvent = new TfgridModuleTwinUpdatedEvent(ctx, item.event)
-  let twin
-  if (twinEvent.isV49) {
-    twin = twinEvent.asV49
-    id = item.event.id
-    twinID = twin.id
-    version = twin.version
-    relay = validateString(ctx, twin.ip.toString())
-    accountID = ss58.codec("substrate").encode(twin.accountId)
-  } else if (twinEvent.isV101) {
-    twin = twinEvent.asV101
-    id = item.event.id
-    twinID = twin.id
-    version = twin.version
-    relay = validateString(ctx, twin.ip.toString())
-    accountID = ss58.codec("substrate").encode(twin.accountId)
-  } else if (twinEvent.isV124) {
-    twin = twinEvent.asV124
-    id = item.event.id
-    twinID = twin.id
-    if (twin.relay) {
-      relay = validateString(ctx, twin.relay?.toString())
+    const twinEvent = new TfgridModuleTwinUpdatedEvent(ctx, item.event)
+    let twin
+    if (twinEvent.isV49) {
+        twin = twinEvent.asV49
+        id = item.event.id
+        twinID = twin.id
+        version = twin.version
+        relay = validateString(ctx, twin.ip.toString())
+        accountID = ss58.codec("substrate").encode(twin.accountId)
+    } else if (twinEvent.isV101) {
+        twin = twinEvent.asV101
+        id = item.event.id
+        twinID = twin.id
+        version = twin.version
+        relay = validateString(ctx, twin.ip.toString())
+        accountID = ss58.codec("substrate").encode(twin.accountId)
+    } else if (twinEvent.isV124) {
+        twin = twinEvent.asV124
+        id = item.event.id
+        twinID = twin.id
+        if (twin.relay) {
+            relay = validateString(ctx, twin.relay?.toString())
+        }
+        accountID = ss58.codec("substrate").encode(twin.accountId)
+        if (twin.pk) {
+            pk = validateString(ctx, '0x' + Buffer.from(twin.pk).toString('hex'));
+        }
     }
-    accountID = ss58.codec("substrate").encode(twin.accountId)
-    if (twin.pk) {
-      pk = validateString(ctx, '0x' + Buffer.from(twin.pk).toString('hex'));
-    }
-  }
 
-  return {
-    id,
-    twinID,
-    version,
-    relay,
-    accountID,
-    pk
-  }
+    return {
+        id,
+        twinID,
+        version,
+        relay,
+        accountID,
+        pk
+    }
 }
 
 interface TwinEvent {
-  id: string
-  twinID: number
-  version: number
-  relay: string
-  accountID: string
-  pk: string
+    id: string
+    twinID: number
+    version: number
+    relay: string
+    accountID: string
+    pk: string
 }
